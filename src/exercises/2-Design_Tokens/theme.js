d2008b50c998333aa3b7248c007c9c8f162df8b4
Daniel Scarpim <danielscarpim@gmail.com>
2019-12-05 10:29:15 +0100

    add custom palette to theme

--- src/exercises/2-Design_Tokens/theme.js
+++ src/exercises/2-Design_Tokens/theme.js
@@ -1,11 +1,22 @@
+const palette = {
+	grey: {
+		100: '#000',
+		200: '#333',
+		400: '#888',
+		700: '#fff'
+	}
+}
+
 const theme = {
 	space: [0, '2px', '4px', '8px', '16px', '32px'],
 	colors: {
-		background: '#fff',
-		text: '#000',
-		primary: '#f39037',
-		secondary: '#509cb7',
-		muted: '#ccc',
+		...palette,
+		background: palette.grey[700],
+		text: palette.grey[200],
+		primary: '#783396',
+		secondary: '#7e7e7e',
+		acceent: '#f8c124',
+		muted: '#f6f6f6',
 	},
 	fonts: {
 		body: `-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif`,
